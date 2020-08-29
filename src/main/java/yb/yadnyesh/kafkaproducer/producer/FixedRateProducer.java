95ca3757b3f2520a6aeb9ba0830fbba90e054da2
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-08-29 12:23:17 +0530

    Producer sending Scheduled messages

---
+++ src/main/java/yb/yadnyesh/kafkaproducer/producer/FixedRateProducer.java
@@ -0,0 +1,26 @@
+package yb.yadnyesh.kafkaproducer.producer;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRateProducer {
+	
+	private Logger log = org.slf4j.LoggerFactory.getLogger(FixedRateProducer.class);
+	
+	@Autowired
+	KafkaTemplate<String, String> kafkaTemplate;
+	
+	private int i = 0;
+	
+	@Scheduled(fixedRate = 1000)
+	public void sendMessage() {
+		i++;
+		log.info("i is: " + i);
+		kafkaTemplate.send("t_fixedrate", "Fixed Rate" + i);
+	}
+
+}
